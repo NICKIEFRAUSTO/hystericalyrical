5288767adfa739c099bd03f60cba183842768296
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-25 10:10:39 -0500

    updated file structure and created models/user.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./Model/userModel.js");
+const User = require("./models/user.js");
 const app = express();
 
 app.use(logger("dev"));
