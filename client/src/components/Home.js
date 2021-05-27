a958363926fb6494029a9a2279d2daa1c23a6576
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-27 09:12:38 -0500

    committing changes for team pull down

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,11 +4,12 @@ import "./style.css"
 function Home(){ 
 return(
 
-<header class="subnav-hero-section">
-  <h1 class="subnav-hero-headline">Hystericalyrical <small>by Frausto, Tomlinson and Vargas</small></h1>
-  <ul class="subnav-hero-subnav" >
-    <li><a href="#">Take Action!</a></li>
-    <li><a href="#" class="is-active">Carpe Diem!</a></li>
+<header className="subnav-hero-section">
+  <h1 className="subnav-hero-headline">Hystericalyrical </h1>
+  <h3 className="subnav-hero-headline"> by Frausto, Tomlinson and Vargas</h3>
+  <ul className="subnav-hero-subnav" >
+  <li><a href="#">Login!</a></li>
+  <li><a href="#" className="is-active">PLAY!</a></li>
     <li><a target="_blank" href="https://zurb.com/responsive">Just do it!</a></li>
   </ul>
   
---
+++ routes/login-out.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const sessionChecker= require("../sessionchecker");
+const User = require("../models/user");
+
+// route for Home-Page
+router.get("/", sessionChecker, (req, res) => {
+    res.redirect("/login");
+  });
+
+  router
+  .route("/Signup")
+  .post((req, res) => {
+
+    const user = new User({
+      username: req.body.username,
+      password:req.body.password,
+    });
+    user.save((err, docs) => {
+      if (err) {
+        res.redirect("/Signup");
+      } else {
+          console.log(docs)
+        req.session.user = docs;
+        res.redirect("/home");
+      }
+    });
+  });
+
+  module.exports = router;
