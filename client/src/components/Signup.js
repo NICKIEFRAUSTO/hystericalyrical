0fa633e4859654ff5f37227fcf3f79417cd5a7bc
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-05-28 09:35:19 -0500

    minor fix

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,49 +1,44 @@
 import React, { useState } from "react";
 import API from "../util/api";
 
-
 function Signup() {
   const [formObject, setFormObject] = useState({
-    username:"", 
-    password:""
-  })
+    username: "",
+    password: "",
+  });
 
-
-  const handleFormSubmit = e => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log("username is " + formObject.username);
     console.log("password is " + formObject.password);
-  API.saveUser (formObject)
+    API.saveUser(formObject);
   };
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
+    setFormObject({ ...formObject, [name]: value });
+  }
 
   return (
     <div>
-      <form className="form">{handlesubmit}
-          <input
-            onChange={handleInputChange}
-            name="username"
-            type="text"
-            placeholder="UserName"
-          />
-          <input
-            onChange={handleInputChange}
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          
-          <button onClick={handleFormSubmit}>Submit</button>
-        </form>
-    </div>      
+      <form className="form">
+        <input
+          onChange={handleInputChange}
+          name="username"
+          type="text"
+          placeholder="UserName"
+        />
+        <input
+          onChange={handleInputChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+
+        <button onClick={handleFormSubmit}>Submit</button>
+      </form>
+    </div>
   );
 }
-                        
-        
 
 export default Signup;
-
