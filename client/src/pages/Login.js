1d0010b5ab19685e85dbf5b432aa9fee5c6fe0d1
NICKIEFRAUSTO <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-06-10 20:39:17 -0500

    removed duplicate pages; Home, Signup, and Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,38 +1,5 @@
-// import React, { useState } from "react";
 
 
 
 
-// function Login(){ 
-//   const [values, setValues]= useState({
-//     username: "",
-//     password: "",
-//   })
-
-// const handleUserNameInputChange = (event)=>{
-//   setValues({...values, username: event.target.value})
-// }
-// const handlePasswordInputChange = (event)=>{
-//   setValues({...values, password: event.target.value})
-// }
-// const handleSubmit=(event)=>{
-//   event.preventDefault();
-  
-// };
-
-
-//   return(
-//     <form className ="form-container" >
-// <form className = "login"
-  
-// </form>
-
-
-
-//   )
-// }
-
-
-
-// export default Login;
 
